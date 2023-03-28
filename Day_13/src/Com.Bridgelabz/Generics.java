787454caaba85660991865310b4358d41118e778
maheshnehere <maheshnehere93@gmail.com>
2023-03-28 19:05:32 +0530

    More than 3

--- Day_13/src/Com.Bridgelabz/Generics.java
+++ Day_13/src/Com.Bridgelabz/Generics.java
@@ -1,13 +1,15 @@
 public class Generics{
 
-    public String maximum(String a, String b, String c){
-        String max = a;
-  
+    public <M extends Comparable > M maximum(M a, M b, M c , M d){
+        M max = a;
+
         if(b.compareTo(a) > 0){
             max = b;
         }else if (c.compareTo(max) > 0){
             max = c;
+        }else if (d.compareTo(max) > 0){
+            max = d;
         }
         return max;
-    }
+    } 
 }
