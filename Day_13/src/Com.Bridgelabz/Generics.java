f8b7d4813c6f09d18f197e8c4abd7185dc6a8fee
maheshnehere <maheshnehere93@gmail.com>
2023-03-28 18:52:43 +0530

    Find max Integer

--- Day_13/src/Com.Bridgelabz/Generics.java
+++ Day_13/src/Com.Bridgelabz/Generics.java
@@ -1,8 +1,8 @@
 public class Generics{
 
-    public <M extends Comparable> M maximum(M a, M b, M c){
-        M max = a;
-
+    public Integer maxInt(Integer a, Integer b, Integer c){
+        Integer max = a;
+  
         if(b.compareTo(a) > 0){
             max = b;
         }else if (c.compareTo(max) > 0){
